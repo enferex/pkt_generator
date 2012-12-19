c65802c7e3f9997cd3224e374729a93722753d5b
Matt Davis <mattdavis9@gmail.com>
2012-12-20 09:43:46 +1100

    Update finderr script

--- scripts/finderrs.sh
+++ scripts/finderrs.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/sh
+#!/bin/sh
 
-for i in `find tests/ -name '*.log'; do wc -l $i; done
+for i in `find tests/ -name '*.log'`; do wc -l $i; done
+
+echo ""
+
+for i in `find tests/ -name '[0-9]*p_[0-9]*b' -type d`
+do
+	diff $i/*.before $i/*.after | grep -ie "\(error\|fail\|loss\|drop\)"
+done
