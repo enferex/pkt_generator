f3cb85ea0c62786fa698c5da4414f5064fd9d1ea
Matt Davis <mattdavis9@gmail.com>
2013-01-17 12:00:57 +1100

    Update with before and after stats

--- scripts/server.sh
+++ scripts/server.sh
@@ -5,7 +5,6 @@ if [ $# == 0 ]; then
 	exit
 fi
 
-
 npkts=$1
 pktsize=$2
 host=$3
@@ -15,7 +14,16 @@ dir=tests/send/${npkts}p_${pktsize}b/
 echo "Creating directory $dir..."
 mkdir -p $dir
 
-sudo ethtool -S $dev > $dir/$dev.before
+# Before test
+sudo ethtool -S $dev > $dir/ethtool.before
+sudo ifconfig $dev   > $dir/ifconfig.before
+
+# Test
 ./pkt_generator -s $host -i $dev -p $npkts -b $pktsize 
-sudo ethtool -S $dev > $dir/$dev.after
+
+# After test
+sudo ethtool -S $dev    > $dir/ethtool.after
+sudo ifconfig $dev      > $dir/ifconfig.after
+wc -l pkt_generator.log > $dir/userland_send.count
+
 mv pkt_generator.log $dir/send_to_${host}.log
